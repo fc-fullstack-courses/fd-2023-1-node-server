244a33182a0ece77745cc938085f80b9ac50dfb4
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-25 15:59:49 +0300

    Add page serving by url

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const http = require('http');
 const fs = require('fs').promises;
 
 const server = http.createServer(async function (request, response) {
-  console.log(request);
-  // console.log(response);
+  console.log(request.url);
+  console.log(request.method);
 
-  const htmlText = await fs.readFile('./views/index.html', 'utf-8');
+  if (request.method === 'GET') {
+    if (request.url === '/' || request.url === '/index.html') {
+      const htmlMainPage = await fs.readFile('./views/index.html', 'utf-8');
 
-  response.end(htmlText);
+      response.end(htmlMainPage);
+    } else if (request.url === '/contacts.html') {
+      const contactsText = await fs.readFile('./views/contacts.html', 'utf-8');
+
+      response.end(contactsText);
+    }else if (request.url === '/about.html') {
+      const aboutText = await fs.readFile('./views/about.html', 'utf-8');
+
+      response.end(aboutText);
+    }
+  }
 });
 
 const PORT = 5000;
