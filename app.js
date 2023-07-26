d5f51b9952dbc76cc65593b5a3412454ace93613
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-26 15:17:34 +0300

    Add POST user basic example

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const yup = require('yup');
 const app = express();
 
-const users = ['User 1', 'User 2', 'User 3'];
+const users = [];
 
 app.get('/', function (request, response) {
   response.end(`Your method is ${request.method} and path is ${request.path}`);
@@ -11,6 +11,46 @@ app.get('/', function (request, response) {
 app.get('/users', (req, res) => {
   res.send(users);
 });
+
+// миддлвер, который берет и ложит JSON данные в req.body в дальнейших миддлверах / обработчиках
+const bodyParser = express.json();
+
+const USER_CREATION_SCHEMA = yup.object({
+  email: yup.string().email().required(),
+  password: yup
+    .string()
+    .matches(/^[a-zA-Z0-9!@#$%^&*]{8,32}$/)
+    .required(),
+});
+
+app.post(
+  '/users',
+  bodyParser,
+  async (req, res, next) => {
+    try {
+      const user = await USER_CREATION_SCHEMA.validate(req.body);
+      req.user = user;
+      next();
+    } catch (error) {
+      res.send('Error, invalid user data');
+    }
+  },
+  async (req, res) => {
+    /*
+    1. распарсить JSON данные
+    2. проверить их на корректность
+    3. сохранить нового юзера (при этом подкинуть ему id)
+    3.5 ОПЦИОНАЛЬНО создать сессию
+    4. Отправить данные на клиент
+  */
+    const { user } = req;
+
+    user.id = Date.now();
+    users.push(user);
+
+    res.send(user);
+  }
+);
 
 app.get(
   '/test',
