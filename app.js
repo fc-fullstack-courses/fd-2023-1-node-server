1c54fc998c40add6954ca669780f4431f7c5fccc
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-26 14:47:37 +0300

    Add basic middlewares example

--- app.js
+++ app.js
@@ -9,9 +9,35 @@ app.get('/', function (request, response) {
 }); // GET request method
 
 app.get('/users', (req, res) => {
-
   res.send(users);
 });
+
+app.get(
+  '/test',
+  (req, res, next) => {
+    console.log('first handler');
+    req.secret = 'secret data';
+    next(); // обработчик выполнил все задачи, можно запускать следующий в цепочке
+  },
+  (req, res, next) => {
+    if (Math.random() > 0.5) {
+      console.log('second handler');
+      next();
+    } else {
+      res.send('middleware ended cycle prematurely');
+    }
+  },
+  (req, res, next) => {
+    console.log('third handler');
+    console.log(req.secret);
+    next();
+  },
+  (req, res, next) => {
+    console.log('last handler');
+
+    res.send('all handlers worked');
+  }
+);
 
 // app.post(); // POST request method
 // app.put(); // PUT request method
