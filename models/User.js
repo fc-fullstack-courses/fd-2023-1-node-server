7527e33e4eef4f6cdcd4fb28b33155d1d24d1904
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-26 16:54:55 +0300

    Add basic user model example

---
+++ models/User.js
@@ -0,0 +1,92 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+// путь к JSON файлу пользователей
+const userDbPath = path.resolve(__dirname, '..', 'db', 'users.json');
+
+class User {
+  constructor(userData) {
+    const { email, password } = userData;
+
+    this.email = email;
+    this.password = password;
+
+    return Promise.resolve(this);
+  }
+
+  static async create(userData) {
+    const userDBString = await fs.readFile(userDbPath, 'utf-8');
+
+    const userDB = userDBString ? JSON.parse(userDBString) : [];
+
+    const newUser = await new User(userData);
+
+    newUser.id = Date.now();
+
+    userDB.push(newUser);
+
+    await fs.writeFile(userDbPath, JSON.stringify(userDB, null, 4));
+
+    return newUser;
+  }
+
+  static async findAll() {
+    const userDBString = await fs.readFile(userDbPath, 'utf-8');
+
+    const userDB = userDBString ? JSON.parse(userDBString) : [];
+
+    return userDB;
+  }
+
+  static async findById(id) {
+    const userDBString = await fs.readFile(userDbPath, 'utf-8');
+
+    const userDB = userDBString ? JSON.parse(userDBString) : [];
+
+    const foundUser = userDB.find((user, index, arr) => user.id === id);
+
+    if (foundUser) {
+      return foundUser;
+    } else {
+      throw new Error('User not found');
+    }
+  }
+
+  static async deleteById(id) {
+    const userDBString = await fs.readFile(userDbPath, 'utf-8');
+
+    const userDB = userDBString ? JSON.parse(userDBString) : [];
+
+    const foundUser = userDB.find((user, index, arr) => user.id === id);
+
+    const newUserDB = userDB.filter((user) => user.id !== id);
+
+    await fs.writeFile(userDbPath, JSON.stringify(newUserDB, null, 4));
+
+    return foundUser;
+  }
+
+  static async updateById(id, newValues) {
+    const userDBString = await fs.readFile(userDbPath, 'utf-8');
+
+    const userDB = userDBString ? JSON.parse(userDBString) : [];
+
+    const foundUser = userDB.find((user, index, arr) => user.id === id);
+
+    if (foundUser) {
+      const userValuesToUpdate = Object.entries(newValues);
+
+      for (const [key, value] of userValuesToUpdate) {
+        foundUser[key] = value;
+      }
+
+      await fs.writeFile(userDbPath, JSON.stringify(userDB, null, 4));
+
+      return foundUser;
+    } else {
+      throw new Error('User not found');
+    }
+  }
+}
+
+module.exports = User;
