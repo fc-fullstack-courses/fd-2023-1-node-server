18c7248cba1cd15c759b23f5caf91b17b951f8f8
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-27 14:46:54 +0300

    Cleanup app.js

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const app = express();
 // миддлвер, который берет и ложит JSON данные в req.body в дальнейших миддлверах / обработчиках
 const bodyParser = express.json();
 
-app.get('/', function (request, response) {
-  response.end(`Your method is ${request.method} and path is ${request.path}`);
-}); // GET request method
-
 app.get('/users', UserController.getUsers);
+
+app.post('/users', bodyParser, validateUser, UserController.createUser);
 
 app.get('/user', UserController.getUserQuery);
 
@@ -23,40 +21,6 @@ app.put('/users/:userId', bodyParser, UserController.updateUser);
 app.get('/users/:userId/messages/:messageId', async (req, res, next) => {
   res.send(req.params);
 });
-
-app.post('/users', bodyParser, validateUser, UserController.createUser);
-
-app.get(
-  '/test',
-  (req, res, next) => {
-    console.log('first handler');
-    req.secret = 'secret data';
-    next(); // обработчик выполнил все задачи, можно запускать следующий в цепочке
-  },
-  (req, res, next) => {
-    if (Math.random() > 0.5) {
-      console.log('second handler');
-      next();
-    } else {
-      res.send('middleware ended cycle prematurely');
-    }
-  },
-  (req, res, next) => {
-    console.log('third handler');
-    console.log(req.secret);
-    next();
-  },
-  (req, res, next) => {
-    console.log('last handler');
-
-    res.send('all handlers worked');
-  }
-);
-
-// app.post(); // POST request method
-// app.put(); // PUT request method
-// app.patch(); // PATCH request method
-// app.delete(); // DELETE request method
 
 const PORT = 5000;
 
